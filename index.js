080d0b707a672c45eaa04bd58d6a1366607774d9
AJ ONeal <aj@savvi.legal>
2022-10-13 16:20:53 -0600

    feat: mercury.submit(application)

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ module.exports = require("./lib/mercury-demo.js")
 
 // these typedef reexports will be available to dependent packages
 /**
+ * @typedef {import('./lib/mercury-demo.js').JobTitleOption} JobTitleOption
+ * @typedef {import('./lib/mercury-demo.js').OperationOption} OperationOption
+ * @typedef {import('./lib/mercury-demo.js').PepOption} PepOption
+ * @typedef {import('./lib/mercury-demo.js').Application} Application
+ * @typedef {import('./lib/mercury-demo.js').BusinessAbout} BusinessAbout
+ * @typedef {import('./lib/mercury-demo.js').BusinessContact} BusinessContact
+ * @typedef {import('./lib/mercury-demo.js').BusinessFormation} BusinessFormation
+ * @typedef {import('./lib/mercury-demo.js').BusinessDetails} BusinessDetails
+ * @typedef {import('./lib/mercury-demo.js').BusinessOwner} BusinessOwner
  */
